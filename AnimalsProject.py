6184fb7b130151a3fb018e1fb568b66fe09f2e41
GeorgKutateladze <georgerailz@gmail.com>
2018-02-24 16:24:09 +0300

    Добавлен объект place, который объединяет всех животных по месту жительства. Добавлены персональные классы для животных, включающие в себя name и sound.

--- AnimalsProject.py
+++ AnimalsProject.py
@@ -1,35 +1,58 @@
-class Animal:
-    def __init__(self, name):
-        self.name = name
-    def get_name(self):
-        return(self.name)
+class Farm_Animals:
+    place = "farm"
 
-class Two_legs(Animal):
-    def __init__(self, name, legs = 2):
-        super().__init__(name)
+class Bird(Farm_Animals):
+    beak = "beak"
+    def __init__(self, wings, legs):
+        self.wings = wings
         self.legs = legs
-    def bird(self):
-        print(self.name, "has", self.legs, "legs.")
 
-class Four_legs(Animal):
-    def __init__(self, name, legs = 4):
-        super().__init__(name)
+class Ungulates(Farm_Animals):
+    hoofs = "hoofs"
+    horns = "horns"
+    def __init__(self, legs):
         self.legs = legs
-    def cattle(self):
-        print(self.name, "has", self.legs, "legs.")
 
-duck = Two_legs("Duck")
-chicken = Two_legs("Chicken")
-goose = Two_legs("Goose")
-duck.bird()
-chicken.bird()
-goose.bird()
+class Cow(Ungulates):
+    name = "Cow"
+    sound = "Moo!"
+class Pig(Ungulates):
+    name = "Pig"
+    sound = "Oink!"
+class Sheep(Ungulates):
+    name = "Sheep"
+    sound = "Mee!"
+class Goat(Ungulates):
+    name = "Goat"
+    sound = "Bee!"
 
-cow = Four_legs("Cow")
-goat = Four_legs("Goat")
-pig = Four_legs("Pig")
-sheep = Four_legs("Sheep")
-cow.cattle()
-goat.cattle()
-pig.cattle()
-sheep.cattle()
+class Chicken(Bird):
+    name = "Chicken"
+    sound = "Cluck!"
+class Goose(Bird):
+    name = "Goose"
+    sound = "Cackle!"
+class Duck(Bird):
+    name = "Duck"
+    sound = "Quack!"
+
+
+chicken = Chicken(2, 2)
+goose = Goose(2, 2)
+duck = Duck(2, 2)
+print("")
+print("%s lives on %s. It has %s, %s wings and %s legs. %s says %s" % (chicken.name, chicken.place, chicken.beak, chicken.wings, chicken.legs, chicken.name, chicken.sound))
+print("%s lives on %s. It has %s, %s wings and %s legs. %s says %s" % (goose.name, goose.place, goose.beak, goose.wings, goose.legs, goose.name, goose.sound))
+print("%s lives on %s. It has %s, %s wings and %s legs. %s says %s" % (duck.name, duck.place, duck.beak, duck.wings, duck.legs, duck.name, duck.sound))
+
+
+
+cow = Cow(4)
+goat = Goat(4)
+pig = Pig(4)
+sheep = Sheep(4)
+print("")
+print("%s lives on %s. It has %s legs, %s and %s. %s says %s" % (cow.name, cow.place, cow.legs, cow.horns, cow.hoofs, cow.name, cow.sound))
+print("%s lives on %s. It has %s legs, %s and %s. %s says %s" % (goat.name, goat.place, goat.legs, goat.horns, goat.hoofs, goat.name, goat.sound))
+print("%s lives on %s. It has %s legs and %s, bit it doesn't have %s. %s says %s" % (pig.name, pig.place, pig.legs, pig.hoofs, pig.horns, pig.name, pig.sound))
+print("%s lives on %s. It has %s legs and %s, bit it doesn't have %s. %s says %s" % (sheep.name, sheep.place, sheep.legs, sheep.hoofs, sheep.horns, sheep.name, sheep.sound))
